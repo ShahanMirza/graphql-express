5071c3d362d027ebc3c879ec46e65465bcbd104a
MuhammadShahan <muhammadshahan@MuhammadShahans-MacBook-Air.local>
2022-06-30 19:07:21 +0500

    file system comment

--- Server.js
+++ Server.js
@@ -5,7 +5,7 @@ const db = require('./db');
 
 const port = 9000;
 const app = express();
-
+//file system
 const fs = require('fs')
 const typeDefs = fs.readFileSync('./schema.graphql',{encoding:'utf-8'})
 const resolvers = require('./resolvers')
